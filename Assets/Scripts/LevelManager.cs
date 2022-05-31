47cb5a11c52270489e9d257f5725d5bc534d5b04
min3nc <lrtsantander@gmail.com>
2022-05-30 21:18:03 -0400

    v0.2.1
    -inciado script de levelmanager

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour
+{
+    public Button Levels;
+    
+
+    public void GoNextLevel()
+    {
+        SceneManager.LoadScene("Level2", LoadSceneMode.Single);
+    }
+}
