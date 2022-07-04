59295bd3c0281e046bf259088b40d268de270a50
Majin <majinflores@gmail.com>
2022-07-03 22:49:37 -0400

    v.1.0
    Examen Taller uwu

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -8,84 +8,85 @@ public class Move : MonoBehaviour
 {
     public int speed, jump;
     private float X, tiempoAire;
-    public Rigidbody2D player;
+    public bool isPlayerOne;
+    public Rigidbody2D player, player2;
     public LayerMask Floor;
-    public GameObject origin, origin1, origin2, originUP1, originUP2, Hand, handCheck;
+    public GameObject origin, origin1, Hand, handCheck;
     public Animator animator;
     float inputHorizontal;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
+        player = GetComponent<Rigidbody2D>();
+        player2 = GetComponent<Rigidbody2D>();
+
     }
 
     public void Update()
     {
-        if(Input.GetKey(KeyCode.Space))
+        if(isPlayerOne)
+        {
+            catOne();
+        }
+
+        else
+        {
+            catTwo();
+        }
+    }
+
+    public void catOne()
+    {
+        if (Input.GetKey(KeyCode.W))
         {
             animator.ResetTrigger("isNotJumping");
             animator.SetTrigger("isJumping");
         }
-        if(Input.GetKeyUp(KeyCode.Space))
+        if (Input.GetKeyUp(KeyCode.W))
         {
             animator.ResetTrigger("isJumping");
             animator.SetTrigger("isNotJumping");
         }
-        
-        if(Input.GetKeyDown(KeyCode.A))
+
+        if (Input.GetKey(KeyCode.A))
         {
             animator.SetFloat("isMoving", 3);
+            transform.position += Vector3.left * speed * Time.deltaTime;
         }
 
-        if(Input.GetKeyUp(KeyCode.A))
+        if (Input.GetKeyUp(KeyCode.A))
         {
             animator.SetFloat("isMoving", 0);
+          
         }
 
-        if(Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKey(KeyCode.D))
         {
             animator.SetFloat("isMoving", 3);
+            transform.position += Vector3.right * speed * Time.deltaTime;
+
         }
 
-        if(Input.GetKeyUp(KeyCode.D))
+        if (Input.GetKeyUp(KeyCode.D))
         {
             animator.SetFloat("isMoving", 1);
+            
         }
 
-        if(Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            animator.SetFloat("isMoving", 3);
-        }
-
-        if(Input.GetKeyUp(KeyCode.LeftArrow))
-        {
-            animator.SetFloat("isMoving", 0);
-        }
-
-        if(Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            animator.SetFloat("isMoving", 3);
-        }
-
-        if(Input.GetKeyUp(KeyCode.RightArrow))
-        {
-            animator.SetFloat("isMoving", 1);
-        }
-
-        player.velocity = new Vector3(Input.GetAxisRaw("Horizontal") * speed, player.velocity.y, 0);
 
         RaycastHit2D raycastForFloor = Physics2D.Raycast(origin.transform.position, Vector2.down, 0.52f, Floor);
         RaycastHit2D raycastForFloor1 = Physics2D.Raycast(origin1.transform.position, Vector2.down, 0.52f, Floor);
-        RaycastHit2D raycastForFloor2 = Physics2D.Raycast(origin2.transform.position, Vector2.down, 0.52f, Floor);
+        
 
         Debug.DrawRay(origin.transform.position, Vector2.down, Color.blue);
         Debug.DrawRay(origin1.transform.position, Vector2.down, Color.blue);
-        Debug.DrawRay(origin2.transform.position, Vector2.down, Color.blue);
+        
 
-        if (raycastForFloor == true || raycastForFloor1 == true || raycastForFloor2 == true)
+        if (raycastForFloor == true || raycastForFloor1 == true)
         {
             tiempoAire = 0;
-           
+
         }
 
         else
@@ -93,46 +94,23 @@ public class Move : MonoBehaviour
             tiempoAire += Time.deltaTime;
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.W))
         {
-            if(raycastForFloor == true || raycastForFloor1 == true || raycastForFloor2 == true)
+            if (raycastForFloor == true || raycastForFloor1 == true)
             {
                 player.velocity = new Vector2(player.velocity.x, jump);
                 Debug.DrawRay(origin.transform.position, Vector2.down, Color.red, Floor);
                 Debug.DrawRay(origin1.transform.position, Vector2.down, Color.red, Floor);
-                Debug.DrawRay(origin2.transform.position, Vector2.down, Color.red, Floor);
+                
             }
 
             else
             {
-                if(tiempoAire < 0.25f)
+                if (tiempoAire < 0.25f)
                 {
                     player.velocity = new Vector2(player.velocity.x, jump);
                 }
             }
-
-        }
-
-        RaycastHit2D raycastD = Physics2D.Raycast(originUP1.transform.position, Vector2.up, 0.52f, Floor);
-        RaycastHit2D raycastI = Physics2D.Raycast(originUP2.transform.position, Vector2.up, 0.52f, Floor);
-
-        Debug.DrawRay(originUP1.transform.position, Vector2.up, Color.blue);
-        Debug.DrawRay(originUP2.transform.position, Vector2.up, Color.blue);
-
-
-        if (raycastI && !raycastD)
-        {
-            transform.position += new Vector3(0.3f, 0);
-            Debug.DrawRay(originUP1.transform.position, Vector2.up, Color.red);
-            Debug.DrawRay(originUP2.transform.position, Vector2.up, Color.blue);
-
-        }
-
-        else if(!raycastI && raycastD)
-        {
-            transform.position -= new Vector3(0.3f, 0);
-            Debug.DrawRay(originUP1.transform.position, Vector2.up, Color.blue);
-            Debug.DrawRay(originUP2.transform.position, Vector2.up, Color.red);
 
         }
 
@@ -141,7 +119,108 @@ public class Move : MonoBehaviour
 
         if (raycastForHandle.collider && raycastForHandle.collider.tag == "Box")
         {
-            if (Input.GetKey(KeyCode.L))
+            if (Input.GetKey(KeyCode.F))
+            {
+                raycastForHandle.collider.gameObject.transform.parent = handCheck.transform;
+                raycastForHandle.collider.gameObject.transform.position = handCheck.transform.position;
+                raycastForHandle.collider.gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
+            }
+
+            else
+            {
+                raycastForHandle.collider.gameObject.transform.parent = null;
+                raycastForHandle.collider.gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+
+            }
+        }
+    }
+
+    public void catTwo()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            animator.ResetTrigger("isNotJumping");
+            animator.SetTrigger("isJumping");
+        }
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            animator.ResetTrigger("isJumping");
+            animator.SetTrigger("isNotJumping");
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            animator.SetFloat("isMoving", 3);
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            animator.SetFloat("isMoving", 0);
+
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            animator.SetFloat("isMoving", 3);
+            transform.position += Vector3.right * speed * Time.deltaTime;
+
+        }
+
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            animator.SetFloat("isMoving", 1);
+
+        }
+
+
+        RaycastHit2D raycastForFloor = Physics2D.Raycast(origin.transform.position, Vector2.down, 0.52f, Floor);
+        RaycastHit2D raycastForFloor1 = Physics2D.Raycast(origin1.transform.position, Vector2.down, 0.52f, Floor);
+        
+
+        Debug.DrawRay(origin.transform.position, Vector2.down, Color.blue);
+        Debug.DrawRay(origin1.transform.position, Vector2.down, Color.blue);
+        
+
+        if (raycastForFloor == true || raycastForFloor1 == true)
+        {
+            tiempoAire = 0;
+
+        }
+
+        else
+        {
+            tiempoAire += Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (raycastForFloor == true || raycastForFloor1 == true)
+            {
+                player.velocity = new Vector2(player.velocity.x, jump);
+                Debug.DrawRay(origin.transform.position, Vector2.down, Color.red, Floor);
+                Debug.DrawRay(origin1.transform.position, Vector2.down, Color.red, Floor);
+             
+            }
+
+            else
+            {
+                if (tiempoAire < 0.25f)
+                {
+                    player.velocity = new Vector2(player.velocity.x, jump);
+                }
+            }
+
+        }
+
+       
+
+        RaycastHit2D raycastForHandle = Physics2D.Raycast(handCheck.transform.position, Vector2.right, 0.10f);
+        Debug.DrawRay(handCheck.transform.position, Vector2.right, Color.cyan);
+
+        if (raycastForHandle.collider && raycastForHandle.collider.tag == "Box")
+        {
+            if (Input.GetKey(KeyCode.M))
             {
                 raycastForHandle.collider.gameObject.transform.parent = handCheck.transform;
                 raycastForHandle.collider.gameObject.transform.position = handCheck.transform.position;
@@ -156,7 +235,6 @@ public class Move : MonoBehaviour
             }
         }
 
-
     }
 
     void FixedUpdate()
@@ -164,14 +242,30 @@ public class Move : MonoBehaviour
         inputHorizontal = Input.GetAxisRaw("Horizontal");
 
 
-        if(inputHorizontal > 0)
+       if(isPlayerOne)
         {
-            gameObject.transform.localScale = new Vector3(7, 7, 7);
+            if (Input.GetKey(KeyCode.D))
+            {
+                gameObject.transform.localScale = new Vector3(7, 7, 7);
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                gameObject.transform.localScale = new Vector3(-7, 7, 7);
+            }
         }
 
-        if(inputHorizontal < 0)
+        if (!isPlayerOne)
         {
-            gameObject.transform.localScale = new Vector3(-7, 7, 7);
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                gameObject.transform.localScale = new Vector3(7, 7, 7);
+            }
+
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                gameObject.transform.localScale = new Vector3(-7, 7, 7);
+            }
         }
 
     }
--- Assets/Scripts/NextLevelGoal.cs
+++ Assets/Scripts/NextLevelGoal.cs
@@ -9,44 +9,23 @@ public class NextLevelGoal : MonoBehaviour
 {
     public Button Levels;
     public CanvasGroup nextLevelScreen;
+    public GameObject victoryCanvas;
 
+    public void Start()
+    {
+        nextLevelScreen.alpha = 0;
+        victoryCanvas.SetActive(false);
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.CompareTag("Player"))
+        if(other.CompareTag("Player1") || other.CompareTag("Player2"))
         {
             nextLevelScreen.alpha = 1;
+            victoryCanvas.SetActive(true);
         }
        
     }
 
 
-    public void GoLeve2()
-    {
-        SceneManager.LoadScene("Level2", LoadSceneMode.Single);
-    }
-    public void GoLeve3()
-    {
-        SceneManager.LoadScene("Level3", LoadSceneMode.Single);
-    }
-    public void GoLeve4()
-    {
-        SceneManager.LoadScene("Level4", LoadSceneMode.Single);
-    }
-
-
-   public void ResetLevel1()
-    {
-        SceneManager.LoadScene("Level1", LoadSceneMode.Single);
-
-    }
-    public void ResetLevel2()
-    {
-        SceneManager.LoadScene("Level2", LoadSceneMode.Single);
-
-    }
-    public void ResetLevel3()
-    {
-        SceneManager.LoadScene("Level3", LoadSceneMode.Single);
-
-    }
+   
 }
